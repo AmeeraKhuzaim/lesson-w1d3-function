36154f3bb7be1bc6e7a8fe10469dec6463311075
AmeeraKhuzaim <49831138+AmeeraKhuzaim@users.noreply.github.com>
2019-04-23 11:12:56 +0300

    Update lab-1.js

---
+++ exercises/lab-1.js
@@ -0,0 +1,17 @@
+function printGreeting(name){
+    //console.log(printGreeting("Slimer"));
+
+    console.log('Hello there '+ name +" !");
+}
+printGreeting("Slimer");
+
+function printCool(name){
+    console.log(name + " is cool");
+}
+printCool("Captain Reynolds");
+
+function calculateCube(num){
+    console.log(num*num*num);
+}
+calculateCube(5);
+
